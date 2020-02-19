9b80111852e435a0e2b1f37d1bb7343db2960a32
eva kramer <eva.kramer91@gmail.com>
2020-02-19 19:37:56 +0000

    controller actions implemented

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    # find the restaurant that we want to add the review to
+    @review = Review.new(review_params)
+    # create new review with review_params
+    @review.restaurant = @restaurant
+    # link the review to a restaurant, connect the foreign keys
+    if @review.save
+      # if true (it passes validations) it will go to redirect
+      redirect_to restaurant_path(@restaurant)
+    else
+      # if false (it does not pass validations)
+      render :new
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
